7951385a7ad89f57306a2d399d5a120dd4f3aaa1
Selenegl2002 <selenegl2002@gmail.com>
2023-11-12 12:01:23 +0100

    corrigiendo los errores

--- prueba1SGL/prueba1SGL/Form1.cs
+++ prueba1SGL/prueba1SGL/Form1.cs
@@ -17,20 +17,24 @@ namespace prueba1SGL
             string textoTelegrama;
             char tipoTelegrama = ' ';
             int numPalabras = 0;
-            double coste;  
+            double coste;
             //Leo el telegrama
             textoTelegrama = txtTelegrama.Text;
             // telegrama urgente?
             if (cbUrgente.Checked)
                 tipoTelegrama = 'u';
+            else
+                tipoTelegrama = 'o';
+
             //Obtengo el número de palabras que forma el telegrama
-            numPalabras = textoTelegrama.Length;
+            string[] palabras = textoTelegrama.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            numPalabras = palabras.Length;
             //Si el telegrama es ordinario
             if (tipoTelegrama == 'o')
                 if (numPalabras <= 10)
-                    coste = 25;
+                    coste = 2.5;
                 else
-                    coste = 0.5 * numPalabras;
+                    coste = 2.5 + 0.5 * numPalabras; //SGL2324
             else
             //Si el telegrama es urgente
             if (tipoTelegrama == 'u')
